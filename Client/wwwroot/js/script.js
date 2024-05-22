18bb301171e94f777775547069532ee22bc8050b
BublikovDev <bublikov.dev@gmail.com>
2024-05-22 07:43:07 +0300

    upd

---
+++ Client/wwwroot/js/script.js
@@ -0,0 +1,6 @@
+﻿function copyToClipboard() {
+    var copyText = document.getElementById("bearerInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+    navigator.clipboard.writeText(copyText.value);
+}
