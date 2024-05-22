c03d3a13c9c5afd90b7d496fc73e28afdd907e2d
BublikovDev <bublikov.dev@gmail.com>
2024-05-22 07:27:38 +0300

    upd

--- Client/wwwroot/js/maps.js
+++ Client/wwwroot/js/maps.js
@@ -27,6 +27,42 @@ function initialize_google() {
     const trafficLayer = new google.maps.TrafficLayer();
 
     trafficLayer.setMap(map_g);
+
+    cords.forEach(function (data) {
+        var marker = new google.maps.Marker({
+            position: { lat: data.lat, lng: data.lng },
+            map: map_g,
+            title: data.info,
+            icon: 'https://i.imgur.com/F2QlQ4v.png'
+        });
+        marker.setMap(map);
+    });
+}
+
+var cords = [
+    { lat: 52.220833, lng: 20.984733 },
+    { lat: 52.220253, lng: 20.916736 },
+    { lat: 52.198525, lng: 21.051978 },
+    { lat: 52.189025, lng: 20.914143 },
+    { lat: 52.231183, lng: 21.127038 },
+    { lat: 52.297869, lng: 21.016926 },
+    { lat: 52.242522, lng: 20.907058 },
+    { lat: 52.241948, lng: 20.994765 },
+    { lat: 52.234843, lng: 21.036877 },
+    { lat: 52.268504, lng: 20.986100 },
+    { lat: 52.235361, lng: 20.972201 },
+    { lat: 52.182608, lng: 20.987679 },
+    { lat: 52.179677, lng: 21.023081 }
+]
+
+
+function addMarkerToMap_g() {
+    var marker = new google.maps.Marker({
+        position: { lat: mag_g.getCenter().lat(), lng: mag_g.getCenter().lng() },
+        map: map_g,
+        title: 'info'
+    });
+    marker.setMap(map_g);
 }
 
 function addMarkers_leaflet(ltd, lng, aqi, className, location) {
