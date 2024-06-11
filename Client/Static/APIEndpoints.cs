95a7e20baf0a1e5957576acfbcc96eee7a58b4d2
BublikovDev <bublikov.dev@gmail.com>
2024-06-11 05:35:00 +0300

    upd

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -5,7 +5,7 @@
 #if DEBUG
         internal const string ServerBaseUrl = "https://localhost:7059";
 #else
-        //Do prod url
+        internal const string ServerBaseUrl = "https://airpollutionserver.azurewebsites.net";
 #endif
 
         internal readonly static string s_refreshToken = $"{ServerBaseUrl}/api/auth/refreshtoken";
@@ -20,11 +20,5 @@
 
         internal readonly static string s_getData = string.Format("{0}/api/data/getdata/{1}", ServerBaseUrl, "{0}");
         internal readonly static string s_putData = string.Format("{0}/api/data/putdata/{1}", ServerBaseUrl, "{0}");
-        //internal readonly static string s_liveStreamGetAllFormat = string.Format("{0}/{1}/GetAllLiveStreams", ServerBaseUrl, LiveStreamRoute);
-        //internal readonly static string s_liveStreamCreateFormat = string.Format("{0}/{1}/CreateLiveStream", ServerBaseUrl, LiveStreamRoute);
-        //internal readonly static string s_liveStreamGetThumbnailByIdFormat = string.Format("{0}/{1}/GetThumbnailUrl/{2}", ServerBaseUrl, LiveStreamRoute, "{0}");
-        //internal readonly static string s_liveStreamStartByIdFormat = string.Format("{0}/{1}/StartLiveStream/start/{2}", ServerBaseUrl, LiveStreamRoute, "{0}");
-        //internal readonly static string s_liveStreamStopByIdFormat = string.Format("{0}/{1}/StopLiveStream/stop/{2}", ServerBaseUrl, LiveStreamRoute, "{0}");
-
     }
 }
